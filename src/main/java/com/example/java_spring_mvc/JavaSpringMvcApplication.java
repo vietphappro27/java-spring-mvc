79c5130767ed274131fbf659349056b1295b8d8a
vietphap27 <phaplon32@gmail.com>
2025-03-14 22:32:42 +0700

    init

---
+++ src/main/java/com/example/java_spring_mvc/JavaSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringMvcApplication.class, args);
+	}
+
+}
