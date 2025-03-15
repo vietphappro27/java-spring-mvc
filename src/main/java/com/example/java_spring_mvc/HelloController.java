395bbd9f1556ae39cf7df62416eb8c5e874a21ec
vietphap27 <phaplon32@gmail.com>
2025-03-15 13:43:00 +0700

    add Dependency: spboot devtools and setup  /admin , /user

--- src/main/java/com/example/java_spring_mvc/HelloController.java
+++ src/main/java/com/example/java_spring_mvc/HelloController.java
@@ -6,8 +6,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String index() {
-        return "Hello, World Vietphap!";
+        return "Hello, World Vietphap! update";
     }
+
+    @GetMapping("/user")
+    public String userpPage() {
+        return "Only user can see this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can see this page";
+    }
+
 }
