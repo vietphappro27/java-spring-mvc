714f923ad8ab7ed5b42ed0ecd8e0f80d18a597c1
vietphap27 <phaplon32@gmail.com>
2025-03-19 16:04:07 +0700

    init

---
+++ src/main/java/com/example/java_spring_mvc/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.java_spring_mvc;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "hello";
+    }
+
+}
