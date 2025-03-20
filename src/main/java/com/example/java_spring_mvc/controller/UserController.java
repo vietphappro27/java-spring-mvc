e7d76c855d3caf7a2727e75f8afb17649beb5298
vietphap27 <phaplon32@gmail.com>
2025-03-20 12:12:25 +0700

    setup JSP

--- src/main/java/com/example/java_spring_mvc/controller/UserController.java
+++ src/main/java/com/example/java_spring_mvc/controller/UserController.java
@@ -1,19 +1,13 @@
 package com.example.java_spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.java_spring_mvc.service.UserService;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
     @RequestMapping("/")
     public String getHomePage() {
-        return "hello.html";
+        return "hello";
     }
 
 }
