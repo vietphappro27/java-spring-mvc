1460600cd01f67630c02ee168121d1b22822e680
vietphap27 <phaplon32@gmail.com>
2025-03-20 17:26:23 +0700

    resetup JSP, integrate JS,Bootstrap

--- src/main/java/com/example/java_spring_mvc/config/WebMvcConfig.java
+++ src/main/java/com/example/java_spring_mvc/config/WebMvcConfig.java
@@ -1,29 +1,34 @@
-// package com.example.java_spring_mvc.config;
+package com.example.java_spring_mvc.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.ViewResolver;
-// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-// import
-// org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-// import org.springframework.web.servlet.view.InternalResourceViewResolver;
-// import org.springframework.web.servlet.view.JstlView;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
-// @Configuration
-// @EnableWebMvc
-// public class WebMvcConfig implements WebMvcConfigurer {
-// @Bean
-// public ViewResolver viewResolver() {
-// final InternalResourceViewResolver bean = new InternalResourceViewResolver();
-// bean.setViewClass(JstlView.class);
-// bean.setPrefix("/WEB-INF/view/");
-// bean.setSuffix(".jsp");
-// return bean;
-// }
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Bean
+    public ViewResolver viewResolver() {
+        final InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/view/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
 
-// @Override
-// public void configureViewResolvers(ViewResolverRegistry registry) {
-// registry.viewResolver(viewResolver());
-// }
-// }
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(viewResolver());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+    }
+}
--- src/main/java/com/example/java_spring_mvc/controller/UserController.java
+++ src/main/java/com/example/java_spring_mvc/controller/UserController.java
@@ -1,13 +1,25 @@
 package com.example.java_spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.java_spring_mvc.service.UserService;
 
 @Controller
 public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping("/")
-    public String getHomePage() {
-        return "hello";
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        // model.addAttribute("message", var);
+        model.addAttribute("message", test);
+        return "hello"; // hello.jsp
     }
 
 }
