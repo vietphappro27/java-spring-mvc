fc1e5fe85da0088442f3336c6afc42c61003b408
vietphap27 <phaplon32@gmail.com>
2025-03-20 10:23:02 +0700

    learn /static

--- src/main/java/com/example/java_spring_mvc/controller/UserController.java
+++ src/main/java/com/example/java_spring_mvc/controller/UserController.java
@@ -1,20 +1,19 @@
 package com.example.java_spring_mvc.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.java_spring_mvc.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+@Controller
 public class UserController {
-    private UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        return "hello.html";
     }
+
 }
